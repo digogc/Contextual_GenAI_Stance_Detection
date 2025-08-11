bf930deaab2dd870d11954f12bd4124e5ef94385
digogc <digo.gcardoso@gmail.com>
2025-08-11 17:42:40 -0300

    obtem_saidas_para_conjunto_teste

--- src/groq_paid_api_example.py
+++ src/groq_paid_api_example.py
@@ -15,7 +15,7 @@ def chat_ai(prompt):
                 "content": prompt,
             }
         ],
-        model="llama3-8b-8192",
+        model="llama-3.3-70b-versatile",
         temperature=1,
     )
 
@@ -31,3 +31,7 @@ if __name__ == "__main__":
 
 # é um serviço excelente, mas é pago a partir de um momento (não sei exatamente qual)
 # vou montar o código pensando no open router, fazer testes com ele, e posteriormente posso alterar na hora de rodar tudo
+
+
+# Acho que o modelo disponibilizado pela Groq não é dos melhores, eu quero utilizar um modelo do gemini
+# Vou pesquisar se existe alguma outra maneira de usar os modelos gratuitos do google
